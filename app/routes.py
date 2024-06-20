67404b0aee89c86fc5b0065373ea69b935b1fb37
Yuto Arigasaki <164603623+yuto141592@users.noreply.github.com>
2024-06-21 05:30:13 +0900

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -24,20 +24,20 @@ main = Blueprint('main', __name__)
 
 
 # firebase_sdk_str = os.getenv('FIRE_BASE_SDK')
-# print(f"Using Firebase SDK file: {firebase_sdk_str}")
-# with open(firebase_sdk_str) as f:
-#     firebaseConfig = json.loads(f.read())
-# firebase = pyrebase.initialize_app(firebaseConfig)
-# print(f"Loaded Firebase config: {firebaseConfig}")
 
-# firebase_config_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-# firebase_config = json.loads(firebase_config_str)
-# cred = credentials.Certificate(firebase_config)
-# firebase_admin.initialize_app(cred, {
-#     'storageBucket': 'imagepicapp.appspot.com'
-# })
-# # firebase = pyrebase.initialize_app(cred)
-# aut = firebase.auth()
+with open("fireBaseSDK.json") as f:
+    firebaseConfig = json.loads(f.read())
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+
+firebase_config_str = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+firebase_config = json.loads(firebase_config_str)
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'imagepicapp.appspot.com'
+})
+
+aut = firebase.auth()
 
 
 # def get_db():
