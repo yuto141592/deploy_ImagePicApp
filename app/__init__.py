0bb59a516efc5fabdcb964b7647a525db4dde15e
yuto141592 <aricchi0208@icloud.com>
2024-06-21 21:10:47 +0900

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,16 @@
 
 from re import M
-import sqlite3
 from flask import Flask, redirect,render_template,request,g,url_for
 import matplotlib.pyplot as plt
 from PIL import Image
 from flask_paginate import Pagination, get_page_parameter
-import os
 from flask import Flask
 from .extensions import db
 from .routes import main
 from .models import User
 from config import Config
-import psycopg2
 from firebase_admin import credentials, initialize_app
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
-import firebase_admin
 from firebase_admin import credentials
 
 def create_app():
@@ -45,21 +41,5 @@ def create_app():
             print("PostgreSQL に接続成功しました！")
         except Exception as e:
             print(f"PostgreSQL への接続中にエラーが発生しました: {e}")
-    # with app.app_context():
-    #     from .routes import main
-    #     app.register_blueprint(main)
-    #     try:
-    #         conn = psycopg2.connect(
-    #             host='dpg-cpo5coo8fa8c73bbo7hg-a',
-    #             port="5432",
-    #             dbname='imagepicapp',
-    #             user='imagepicapp_user',
-    #             password='ZeXYZxkVbbxX7TcmiyyPLMs6EC9HL2NL'
-    #         )
-    #         print("Connection successful")
-    #         conn.close()
-    #     except psycopg2.OperationalError as e:
-    #         print(f"Connection failed: {e}")
-
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,6 @@
 from flask import Blueprint, redirect, url_for, Flask, jsonify, render_template, flash, render_template_string, request,g, session
 from .extensions import db
 from .models import User, Images
-from config import Config
-from re import M
-import matplotlib.pyplot as plt
 from PIL import Image
 from flask_paginate import Pagination, get_page_parameter
 import os
@@ -22,13 +19,6 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 
 main = Blueprint('main', __name__)
 
-
-# firebase_sdk_str = os.getenv('FIRE_BASE_SDK')
-
-# with open("fireBaseSDK.json") as f:
-#     firebaseConfig = json.loads(f.read())
-# firebase = pyrebase.initialize_app(firebaseConfig)
-
 firebase_sdk_str = os.getenv('FIRE_BASE_SDK')
 firebase_sdk = json.loads(firebase_sdk_str)
 firebase = pyrebase.initialize_app(firebase_sdk)
@@ -44,87 +34,6 @@ firebase_admin.initialize_app(cred, {
 aut = firebase.auth()
 
 
-# def get_db():
-#     if 'db' not in g:
-#         # データベースをオープンしてFlaskのグローバル変数に保存
-#         g.db = psycopg2.connect(
-#             host='dpg-cpo5coo8fa8c73bbo7hg-a',
-#             port="5432",
-#             dbname='imagepicapp',
-#             user='imagepicapp_user',
-#             password='ZeXYZxkVbbxX7TcmiyyPLMs6EC9HL2NL'
-#         )
-#         #postgres://imagepicapp_user:ZeXYZxkVbbxX7TcmiyyPLMs6EC9HL2NL@dpg-cpo5coo8fa8c73bbo7hg-a.singapore-postgres.render.com/imagepicapp
-#     return g.db
-
-# @main.route('/')
-# def index():
-
-#     # データベースを開く
-#     con = get_db()
-
-#     cur = con.execute("select count(*) from sqlite_master where TYPE='table' AND name='Images'")
-
-#     for row in cur:
-#         if row[0] == 0:
-#             cur.execute("CREATE TABLE Images(id INTEGER PRIMARY KEY, image_path STRING, image_keyword STRING)")
-
-#             con.commit()
-    
-#     cur = con.execute("select * from Images order by id")
-#     data = cur.fetchall()
-#     con.close()
-
-#     #data = Image.query.order_by(desc(Image.id)).all()
-#     #data = Photo.query.all()
-#     cur = con.execute("select * from Images order by id")
-#     data = cur.fetchall()
-#     con.close()
-
-    
-#     page = request.args.get(get_page_parameter(), type=int, default=1)
-#     res = data[(page - 1)*12: page*12]
-#     pagination = Pagination(page=page, total=len(data),  per_page=12, css_framework='bootstrap4')
-
-
-#     return render_template('list_index.html', data = res, pagination=pagination)
-
-# @main.route('/')
-# def index():
-
-#     # データベースを開く
-#     # con = get_db()
-
-#     # cur = con.execute("select count(*) from sqlite_master where TYPE='table' AND name='Images'")
-
-#     # for row in cur:
-#     #     if row[0] == 0:
-#     #         cur.execute("CREATE TABLE Images(id INTEGER PRIMARY KEY, image_path STRING, image_keyword STRING)")
-
-#     #         con.commit()
-    
-#     # cur = con.execute("select * from Images order by id")
-#     # data = cur.fetchall()
-#     # con.close()
-
-#     #data = Image.query.order_by(desc(Image.id)).all()
-#     data = Photo.query.order_by(Photo.id).all()
-#     # cur = con.execute("select * from Images order by id")
-#     # data = cur.fetchall()
-#     # con.close()
-
-    
-#     page = request.args.get(get_page_parameter(), type=int, default=1)
-#     res = data[(page - 1)*12: page*12]
-#     pagination = Pagination(page=page, total=len(data),  per_page=12, css_framework='bootstrap4')
-
-
-#     return render_template('list_index.html', data = res, pagination=pagination)
-
-# @main.route('/')
-# @login_required
-# def index():
-#     return render_template('index_T.html')
 @main.route('/', methods=['GET', 'POST'])
 def login_f():
     if request.method == 'POST':
@@ -138,7 +47,6 @@ def login_f():
             if user.check_password(password):
 
                 try:
-                    # user = admin_auth.sign_in_with_email_and_password(email, password)
                     user2 = aut.sign_in_with_email_and_password(email, password)
 
                     id_token = user2['idToken']
@@ -169,29 +77,8 @@ def login_f():
 @login_required
 def index_lost():
 
-    # # データベースを開く
-    # con = get_db()
-
-    # cur = con.execute("select count(*) from sqlite_master where TYPE='table' AND name='Images'")
-
-    # for row in cur:
-    #     if row[0] == 0:
-    #         cur.execute("CREATE TABLE Images(id INTEGER PRIMARY KEY, image_path STRING, image_keyword STRING)")
-            
-    #         con.commit()
-    
-    # cur = con.execute("select * from Images order by id")
-    # data = cur.fetchall()
-    # con.close()
-
 
     data = Images.query.filter(Images.user_id==session['user']).order_by(Images.id).all()
-    #data = Photo.query.order_by(Photo.id).all()
-    # del_data = Photo.query.where(Photo.image_path=='https://storage.googleapis.com/imagepicapp.appspot.com/hX2faunUrfhjj7bIbGQQdLKY9TU2/S__91987973_0.jpg?Expires=1718889109&GoogleAccessId=firebase-adminsdk-e4hrb%40imagepicapp.iam.gserviceaccount.com&Signature=HH8hK8wZaRM2fw%2F8k9TwjB%2BLGFYNWedC4rYiqoxbhD1b7am1sK%2FVUNpZ2cy2rc4vslESi3seTdaMYQJqizMvsyUC%2B%2FvVqYq7pG5zL7h1jvMrm1FS3ZRrGX%2BrwcRxLE3VvlGm%2BVXbrp4YsZuGLfBw6MZ1lpvMbUq1Gh%2BDgFzIv4KCHRC9XkyXHYVck9uwoA01ZVJv%2FjHsz4A3b1d6cldYHvRODd48%2BesLQWRwJG1We4oB%2FtBlmYdI01N8aQ3BSsRJGxA9Kqt7jvWq6sgdZGG3Lik4WRR4fS4L5x1BH4iL7i2XJARcrugSyGsA2k3A%2FrPEjbTuytyjmZj9dp632kLNAA%3D%3D').first()
-    # db.session.delete(del_data)
-    # db.session.commit()
-    # db.session.execute("ALTER TABLE Images2 DROP CONSTRAINT Images2_user_id")
-    # db.session.commit()
 
     new_data = []
     for d in data:
@@ -201,10 +88,6 @@ def index_lost():
         file_url = blob.generate_signed_url(timedelta(minutes=15))
         new_l = [file_url, d.image_name, d.image_keyword, d.id]
         new_data.append(new_l)
-
-    # cur = con.execute("select * from Images order by id")
-    # data = cur.fetchall()
-    # con.close()
 
     page = request.args.get(get_page_parameter(), type=int, default=1)
     res = new_data[(page - 1)*12: page*12]
@@ -222,19 +105,14 @@ def result_post():
     file = request.files['example']
     if file:
         file_path = 'app/templates/kabegami' + '/' + file.filename
-        print('filename=', file_path)
-        #file.save(os.path.join('templates/kabegami', file.filename))
+
         file.save(file_path)
         name = f"{session['user']}/{file.filename}"
 
-        # データベースを開く
-        #con = get_db()
+ 
         data2 = Images.query.filter(Images.user_id==session['user']).order_by(Images.id).all()
-
-        #cur = con.execute("select MAX(id) AS max_code from Images")
         data3 = Images.query.order_by(Images.id.desc()).first()
-        #cur2 = con.execute("select * from Images order by id")
-        #data2 = cur2.fetchall()
+
         list = []
         for item in data2:
             list.append(item.image_keyword)
@@ -250,24 +128,13 @@ def result_post():
 
         new_list = sorted(list3)
 
-        # for row in cur:
-        #     if row[0] != None:
-        #         new_code = row[0] + 1
-        #     else:
-        #         new_code = 1
         if data3 != None:
             new_code = data3.id + 1
         else:
             new_code = 1
 
-        # cur.close()
-        # sql = "INSERT INTO Images(id, image_path, image_keyword)values({},'{}','_?/keyword/?_')".format(new_code, name)
-        # con.execute(sql)
-        # con.commit()
         db.session.add(Images(id=new_code, user_id=session['user'], image_path=name, image_keyword='_?/keyword/?_'))
-        db.session.commit()
-
-        
+        db.session.commit()    
 
         bucket = storage.bucket()
         blob = bucket.blob(f"{session['user']}/{file.filename}")
@@ -275,9 +142,6 @@ def result_post():
 
         os.remove(file_path)
 
-        # cur = con.execute("select * from Images where id = {}".format(new_code))
-        # data = cur.fetchall()
-        # con.close()
         data = Images.query.filter(Images.id==new_code, Images.user_id==session['user']).first()
 
         new_data = data
@@ -294,15 +158,9 @@ def result_post():
 @login_required
 def register():
     
-    #con = get_db()
-    # pre_data = Images.query.filter(user_id=session['user']).all()
+
     data = Images.query.filter(Images.user_id==session['user']).filter(Images.image_keyword.like('%_?/keyword/?_%')).all()
-    #sql3 = "select * from Images where image_keyword LIKE '%_?/keyword/?_%'"
-    #cur = con.execute("select *  from Images where id = MAX(id)")
-    # cur = con.execute(sql3)
-    # data = cur.fetchall()
-    
-    
+ 
     data1 = data[0].id
     data1_ = int(data1)
     data2 = data[0].image_path
@@ -311,18 +169,11 @@ def register():
     get_item = request.form.getlist("item")
     new_name = str(get_item[0])
     new_key = str(get_item[1])
-    # sql2 = "DELETE FROM Images WHERE image_keyword LIKE '%_?/keyword/?_%'"
-    # con.execute(sql2)
-    # con.commit()
+
     delImg = Images.query.filter(Images.user_id==session['user']).filter(Images.image_keyword.like('%_?/keyword/?_%')).first()
     db.session.delete(delImg)
     db.session.commit()
-   
-    # sql4 = "insert into Images(id, image_path, image_keyword) values({}, '{}', '{}')".format(data1_, data2_, new_key)
-    # con.execute(sql4)
-    # con.commit()
-    # cur = con.execute("select * from Images order by id")
-    # data = cur.fetchall()
+
 
     db.session.add(Images(id=data1_, user_id=session['user'], image_name=new_name, image_path=data2_, image_keyword=new_key))
     db.session.commit()
@@ -336,7 +187,6 @@ def register():
         file_url = blob.generate_signed_url(timedelta(minutes=15))
         new_l = [file_url, d.image_name, d.image_keyword, d.id]
         new_data.append(new_l)
-    #con.close()
 
     page = request.args.get(get_page_parameter(), type=int, default=1)
     res = new_data[(page - 1)*12: page*12]
@@ -352,15 +202,7 @@ def register():
 @login_required
 def delete_post(id):
 
-    # con = get_db()
 
-    # sql2 = "DELETE FROM Images WHERE id = {}".format(id)
-    # con.execute(sql2)
-    # con.commit()
-
-    # cur = con.execute("select * from Images order by id")
-    # data = cur.fetchall()
-    # con.close()
     file_path = Images.query.filter(Images.user_id==session['user']).filter(Images.id==id).first()
     delPath = file_path.image_path
     bucket = storage.bucket()
@@ -392,13 +234,7 @@ def delete_post(id):
 @main.route("/open_image/<int:id>")
 @login_required
 def open_image(id):
-    # con = get_db()
-    # sql = "select image_path from Images where id={}".format(id)
-    # cur = con.execute(sql)
-    # image = cur.fetchall()
-    # con.close()
 
-    #image = Photo.query.with_entities(Photo.image_path).get(id)
     img_id = id 
     image = Images.query.filter(Images.id==img_id, user_id=session['user']).with_entities(Images.image_path).scalar()
 
@@ -424,11 +260,7 @@ def open_image(id):
 @login_required
 def row_image():
     im = request.form["check"]
-    # con = get_db()
-    # cur = con.execute("select * from Images where image_keyword LIKE '%{}%'".format(im))
-    # data = cur.fetchall() 
-    
-    #con.close()
+
     data = Images.query.filter(Images.user_id==session['user']).filter(Images.image_keyword.like('%{}%'.format(im))).all()
 
     return render_template("images.html", data=data)
@@ -436,11 +268,6 @@ def row_image():
 @main.route("/result2")
 @login_required
 def open_image2():
-    # con = get_db()
-
-    # cur = con.execute("select * from Images order by id")
-    # data = cur.fetchall()
-    # con.close()
 
     data = Images.query.filter(Images.user_id==session['user']).order_by(Images.id).all()
 
@@ -465,10 +292,7 @@ def open_image2():
 @main.route("/list")
 @login_required
 def list_open():
-    # con = get_db()
-    # cur = con.execute("select * from Images order by id")
-    # data = cur.fetchall()
-    # con.close()
+
 
     data = Images.query.filter(Images.user_id==session['user']).order_by(Images.id).all()
 
@@ -481,13 +305,6 @@ def list_open():
 @main.route("/update/<int:id>")
 @login_required
 def update(id):
-    # con = get_db()
-    # sql3 = "select * from Images where id = {}".format(id)
-    
-    # cur = con.execute(sql3)
-    # data = cur.fetchall()
-    # con.close()
-
     data = Images.query.filter(Images.user_id==session['user']).get(id)
     
     return render_template('update.html', data = data)
@@ -496,11 +313,7 @@ def update(id):
 @main.route("/update/<int:id>", methods=["POST"])
 @login_required
 def update_post(id):
-    # con = get_db()
-    # sql3 = "select * from Images where id = {}".format(id)
-    # cur = con.execute(sql3)
-    # data = cur.fetchall()
-    
+
     data = Images.query.filter(Images.user_id==session['user']).get(id)
     
     data1 = data[0].id
@@ -510,20 +323,9 @@ def update_post(id):
    
     new_key = request.form.get("key_word")
 
-    # sql2 = "DELETE FROM Images WHERE id = {}".format(id)
-    # con.execute(sql2)
-    # con.commit()
     db.session.delete(data)
     db.session.commit()
 
-   
-    # sql4 = "insert into Images(id, image_path, image_keyword) values({}, '{}', '{}')".format(data1_, data2_, new_key)
-    # con.execute(sql4)
-    # con.commit()
-    # cur = con.execute("select * from Images order by id")
-    # data = cur.fetchall()
-    
-    # con.close()
     new_img = Images(id=data1_, user_id=session['user'], image_path=data2_, image_keyword=new_key)
     db.session.add(new_img)
     db.session.commit()
@@ -534,11 +336,6 @@ def update_post(id):
 @main.route("/about_index")
 def about():
     return render_template('about_index.html')
-
-
-# @main.route('/')
-# def index():
-#     return render_template('index_T.html')
 
 @main.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -577,7 +374,6 @@ def login():
             if user.check_password(password):
 
                 try:
-                    # user = admin_auth.sign_in_with_email_and_password(email, password)
                     user2 = aut.sign_in_with_email_and_password(email, password)
 
                     id_token = user2['idToken']
