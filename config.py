96e8c634a617fb38480d93094d2088ba41afeb2c
yuto141592 <aricchi0208@icloud.com>
2024-06-21 23:05:25 +0900

    x

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
-    #SECRET_KEY = '8e9066bc94cf595ec3fdb445190128e6'
     FIREBASE_CONFIG = os.getenv('FIRE_BASE_SDK')
-    #FIREBASE_CONFIG = "fireBaseSDK.json"
+
 
